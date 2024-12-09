b3a51cb8a82eba0b3b51889fcaed15284cc4ae58
colayyun <koyun0902@gmail.com>
2024-12-09 15:27:30 +0800

    v2_洪可芸

--- app/src/main/java/tw/edu/pu/s1110054/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1110054/MainActivity.kt
@@ -1,18 +1,44 @@
 package tw.edu.pu.s1110054
 
 import android.content.pm.ActivityInfo
+import android.media.Image
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import tw.edu.pu.s1110054.ui.theme.S1110054Theme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+
+import androidx.compose.ui.platform.LocalContext
+import android.app.Activity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +51,10 @@ class MainActivity : ComponentActivity() {
 
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
-                    Start(m = Modifier.padding(innerPadding))
-
-
+                    Greeting(
+                        name ="2024期末上機考(資管三B洪可芸)",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -35,5 +62,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Start(m: Modifier){
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff95fe95)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column (
+
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ){
+
+
+            Text(
+                text = "$name!",
+                modifier = modifier
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.class_b),
+                contentDescription = "B班",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+            )
+
+            Text(
+                text = "遊戲持續時間 0 秒",
+                modifier = modifier
+            )
+
+
+            Text(
+                text = "您的成績 0 分",
+                modifier = modifier
+            )
+
+            val activity = (LocalContext.current as? Activity)
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Button(
+                    onClick = {
+
+                        activity?.finish()
+                    }
+                ) {
+                    Text("結束App")
+                }
+
+            }
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+            
+
